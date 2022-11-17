2bbc29aba2f138a16cfb01349ac6ad3d2751a898
JanGospodarek <jan.gospo@onet.pl>
2022-11-17 19:35:09 +0100

    add basic dir struct

---
+++ src/components/mainView/main.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Main() {
+  return (
+    <View>
+      <Text>essa essa</Text>
+    </View>
+  );
+}
---
+++ src/components/reuseable/shit.tsx
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Shit() {
+  return (
+    <View>
+      <Text>essa essa</Text>
+    </View>
+  );
+}
